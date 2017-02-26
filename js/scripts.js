600f17ae365b667030ad9d3a6894149044950544
RS <roboszyn@gmail.com>
2017-02-26 02:56:49 +0000

    Exercise 8.7

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'];
+var maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'];
+var allNames = femaleNames.concat(maleNames)
+var newName = prompt('Podaj imię')
+
+if (allNames.indexOf(newName) === -1) {allNames.push(newName), alert('Imię zostało dodane do tablicy: ' + allNames);}
+else if (allNames.indexOf(newName) != -1) {alert('To imię już się znajduje w tablicy');}
+
+console.log('Sprawdzane imię: ' + newName + ' znajduje się na ' + allNames.indexOf(newName) + ' pozycji tablicy');
+console.log(allNames)

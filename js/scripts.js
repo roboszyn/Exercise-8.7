3112c7c778cb9eb3fdb9c2e1c1473e91c1cbf27f
RS <roboszyn@gmail.com>
2017-03-07 02:52:31 +0000

    fixing 'if' code

--- js/scripts.js
+++ js/scripts.js
@@ -4,8 +4,11 @@ var allNames = femaleNames.concat(maleNames)
 var newName = prompt('Podaj imię')
 
 if (allNames.indexOf(newName) === -1) {
-	allNames.push(newName), alert('Imię zostało dodane do tablicy: ' + allNames);}
-else {alert('To imię już się znajduje w tablicy');}
+	allNames.push(newName);
+	alert('Imię zostało dodane do tablicy: ' + allNames);
+} else {
+	alert('To imię już się znajduje w tablicy');
+}
 
 console.log('Sprawdzane imię: ' + newName + ' znajduje się na ' + allNames.indexOf(newName) + ' pozycji tablicy');
-console.log(allNames)
+console.log(allNames);
